fc82e21326a08d0775e930432112f36fddc9fe32
C.C. Miller <christopher.miller@ll.mit.edu>
2017-04-28 08:45:57 -0400

    config cleanup

--- src/js/Config.js
+++ src/js/Config.js
@@ -1,9 +1,5 @@
 var Config={
-	"MODE":"apt",
-	"POINTBUFFER":500,
-	"APIKEY":0,
-	"SOLRROOT":"http://localhost:8080/solr/biblio/",
-	"POLYOPACITY":.85,
-	"CARTO_USER":"cecmcgee",
-	"D_FORMATS_CARTO":["csv","shp","geojson","kml","svg"]
+	"MODE":"apt"
+	,"POINTBUFFER":500
+	,"APIKEY":0
 }
