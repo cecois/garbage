687e7535c693889e521522dfd8b2e24a6313d92d
C.C. Miller <christopher.miller@ll.mit.edu>
2017-05-01 15:58:55 -0400

    slug-sensitive slider hide

--- src/js/BelliesMenu-View.js
+++ src/js/BelliesMenu-View.js
@@ -12,74 +12,85 @@ var BelliesMenuView = Backbone.View.extend({
 // .prep()
 }
 ,render: function(t){
-	var timz = _.uniq(_.map(this.collection.models,(m)=>{
-		return m.get("ts_as_ts");
-	})).sort()
+	console.log("appstate.slug",appState.get("slug"))
+	if(appState.get("slug")=="home" || appState.get("slug")=="about"){
 
-	var inc=null
-	switch(appState.get("slug")) {
-		case "baa":
-		incv = Config.EVENTS.baa.increment
-		break;
-		case "home":
-		incv = Config.EVENTS.baa.increment
-		break;
-		case "about":
-		incv = Config.EVENTS.baa.increment
-		break;
-		case "animebos":
-		incv = Config.EVENTS.animebos.increment
-		break;
-		case "riot":
-		incv = Config.EVENTS.riot.increment
-		break;
-		case "boscalling":
-		incv = Config.EVENTS.boscalling.increment
-		break;
-		default:
-		return null
+		// $(this.el).html('')
+		$(this.el).find("#slider").html('')
+
+	} //slug check
+	else{
+
+		var timz = _.uniq(_.map(this.collection.models,(m)=>{
+			return m.get("ts_as_ts");
+		})).sort()
+
+		var inc=null
+		switch(appState.get("slug")) {
+			case "baa":
+			incv = Config.EVENTS.baa.increment
+			break;
+			case "home":
+			incv = Config.EVENTS.baa.increment
+			break;
+			case "about":
+			incv = Config.EVENTS.baa.increment
+			break;
+			case "animebos":
+			incv = Config.EVENTS.animebos.increment
+			break;
+			case "riot":
+			incv = Config.EVENTS.riot.increment
+			break;
+			case "boscalling":
+			incv = Config.EVENTS.boscalling.increment
+			break;
+			default:
+			return null
+		}
+
+
+		var min = _.min(timz)
+		var max = _.max(timz)
+		var inc = Math.ceil((max-min)/incv)
+
+		var ticks = [min]
+
+		var m=min
+		var i = 0
+		do {
+			var mi =m+incv
+			ticks.push(mi)
+			m=mi;
+			i++
+		}
+		while (i < inc);
+
+		if(appState.get("time")!=='undefined' && appState.get("time")!==null){
+			var sv= parseInt(appState.get("time"))
+		}
+		else
+		{
+			var sv= min
+		}
+
+
+		var t=[]
+
+
+		var slider = $(this.el).find("#slider").slider({
+			id:"slider"
+			,formatter: (v)=>{return moment(v,['X']).format('dd (Do) hh:ss A')}
+			,ticks_snap_bounds:incv/2
+			,ticks: _.uniq(ticks)
+			,tooltip:"show"
+			,value:sv
+		})
+		.on('change',(v)=>{
+			appState.set({time:parseInt(v.value.newValue)})
+		})
+
 	}
-
-
-	var min = _.min(timz)
-	var max = _.max(timz)
-	var inc = Math.ceil((max-min)/incv)
-
-	var ticks = [min]
-
-	var m=min
-	var i = 0
-	do {
-		var mi =m+incv
-		ticks.push(mi)
-		m=mi;
-		i++
-	}
-	while (i < inc);
-
-	if(appState.get("time")!=='undefined' && appState.get("time")!==null){
-		var sv= parseInt(appState.get("time"))
-	}
-	else
-	{
-		var sv= min
-	}
-
-
-	var t=[]
-
-   
-	var slider = $(this.el).find("#slider").slider({
-		id:"slider"
-		,formatter: (v)=>{return moment(v,['X']).format('dd (Do) hh:ss A')}
-		,ticks_snap_bounds:incv/2
-		,ticks: _.uniq(ticks)
-		,tooltip:"show"
-		,value:sv
-	})
-	.on('change',(v)=>{
-		appState.set({time:parseInt(v.value.newValue)})
-	})
 
 
 	return this
